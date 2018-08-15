6dddc1a212f6710ab8374ffe0a3eaf4561291c89
Shelan <shelandebesai@gmail.com>
2018-08-15 11:02:08 -0700

    uncommented

--- pull_practice.rb
+++ pull_practice.rb
@@ -7,32 +7,32 @@
 # One partner should uncomment this method, add and
 # commit the changes and push to GitHub. The other
 # partner should run git pull to retrieve the changes.
-# def duck_noise
-#   puts "quack"
-# end
+def duck_noise
+  puts "quack"
+end
 
-# Task 2: Same as task one, but switch roles
-# def truck_noise
-#   puts "honk"
-# end
+Task 2: Same as task one, but switch roles
+def truck_noise
+  puts "honk"
+end
 
-# Taks 3: Merging Changes
-# One partner should uncomment one method, and the other
-# partner should uncomment the other. Git will merge the two
-# sets of changes together.
+Taks 3: Merging Changes
+One partner should uncomment one method, and the other
+partner should uncomment the other. Git will merge the two
+sets of changes together.
 
-# def robot_noise
-#   puts "beep boop"
-# end
+def robot_noise
+  puts "beep boop"
+end
 
-# def train_noise
-#   puts "choo choo"
-# end
+def train_noise
+  puts "choo choo"
+end
 
-# Task 4: Merge Conflict
-# Both partners should modify the same line in different ways.
-# Git doesn't know what to do in this case, so you'll have to
-# merge the two versions by hand.
+Task 4: Merge Conflict
+Both partners should modify the same line in different ways.
+Git doesn't know what to do in this case, so you'll have to
+merge the two versions by hand.
 def clock_noise
   puts ""
 end
